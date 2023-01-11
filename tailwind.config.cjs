af223396363d7f755af8434dd81a44ee401fc52e
KevinMendieta <kevin@wonderschool.com>
2023-01-11 13:17:38 -0500

    project setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      salmon: {
+        100: '#FF9185',
+        200: '#F04C39',
+      }
+    },
+  },
+  plugins: [],
+}
